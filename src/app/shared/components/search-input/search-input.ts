c70570dca080a24f29e23920c52afa6834ae4889
Christian Algarin <christian35620@gmail.com>
2026-05-18 17:07:57 -0300

    add search input

--- src/app/shared/components/search-input/search-input.ts
+++ src/app/shared/components/search-input/search-input.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
   imports: [],
   templateUrl: './search-input.html',
   styleUrl: './search-input.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchInput {}
